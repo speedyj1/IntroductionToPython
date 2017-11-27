0ba8e14e03b01ae6cf5e87776dfb80548928dcc9
speedyj1 <speedyj1@rose-hulman.edu>
2017-11-27 11:40:16 -0500

    progress

--- src/m5_your_turtles.py
+++ src/m5_your_turtles.py
@@ -2,10 +2,10 @@
 Your chance to explore Loops and Turtles!
 
 Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
+         their colleagues and Jack Speedy.
 """
 ########################################################################
-# TODO: 1.
+# DONE: 1.
 # On Line 5 above, replace  PUT_YOUR_NAME_HERE  with your own name.
 ########################################################################
 
@@ -28,3 +28,25 @@ Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
 #
 #  Don't forget to COMMIT your work by using  VCS ~ Commit and Push.
 ########################################################################
+size = 300
+
+# Do the indented code 13 times.  Each time draws a square.
+for k in range(6):
+
+    # Put the pen down, then draw a square of the given size:
+    blue_turtle.draw_square(size)
+
+    # Move a little below and to the right of where the previous
+    # square started.  Do this with the pen up (so nothing is drawn).
+    blue_turtle.pen_up()
+    blue_turtle.right(45)
+    blue_turtle.forward(6)
+    blue_turtle.left(45)
+
+    # Put the pen down again (so drawing resumes).
+    # Make the size for the NEXT square be 12 pixels smaller.
+    blue_turtle.pen_down()
+    size = size - 12
+
+    red_turtle.draw_square(size)
+window.close_on_mouse_click()
