3800e5e82a36a16b342d5b41fe22d29f19b9b122
speedyj1 <speedyj1@rose-hulman.edu>
2017-11-28 16:13:34 -0500

    progress

--- src/m5_your_turtles.py
+++ src/m5_your_turtles.py
@@ -28,25 +28,21 @@ Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
 #
 #  Don't forget to COMMIT your work by using  VCS ~ Commit and Push.
 ########################################################################
-size = 300
+import rosegraphics as rg
 
-# Do the indented code 13 times.  Each time draws a square.
-for k in range(6):
+window = rg.TurtleWindow()
 
-    # Put the pen down, then draw a square of the given size:
-    blue_turtle.draw_square(size)
+blue_turtle=rg.SimpleTurtle('turtle')
+blue_turtle.pen=rg.Pen('blue',5)
+blue_turtle.speed= 5
 
-    # Move a little below and to the right of where the previous
-    # square started.  Do this with the pen up (so nothing is drawn).
+size=350
+
+for k in range(11):
+    blue_turtle.draw_regular_polygon(3,size)
     blue_turtle.pen_up()
     blue_turtle.right(45)
-    blue_turtle.forward(6)
+    blue_turtle.forward(10)
     blue_turtle.left(45)
-
-    # Put the pen down again (so drawing resumes).
-    # Make the size for the NEXT square be 12 pixels smaller.
     blue_turtle.pen_down()
-    size = size - 12
-
-    red_turtle.draw_square(size)
-window.close_on_mouse_click()
+    size=size - 15
