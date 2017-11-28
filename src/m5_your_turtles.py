c8f27d8819ca180875dfddde4f7e4fdbc49d835e
speedyj1 <speedyj1@rose-hulman.edu>
2017-11-28 16:22:12 -0500

    m5 done

--- src/m5_your_turtles.py
+++ src/m5_your_turtles.py
@@ -34,15 +34,28 @@ window = rg.TurtleWindow()
 
 blue_turtle=rg.SimpleTurtle('turtle')
 blue_turtle.pen=rg.Pen('blue',5)
-blue_turtle.speed= 5
+blue_turtle.speed= 7
 
-size=350
+yellow_turtle=rg.SimpleTurtle('turtle')
+yellow_turtle.pen=rg.Pen('yellow',3)
+yellow_turtle.speed= 5
+
+blue_size=350
+yellow_size=-200
 
 for k in range(11):
-    blue_turtle.draw_regular_polygon(3,size)
+    blue_turtle.draw_regular_polygon(3,blue_size)
     blue_turtle.pen_up()
     blue_turtle.right(45)
     blue_turtle.forward(10)
     blue_turtle.left(45)
     blue_turtle.pen_down()
-    size=size - 15
+    blue_size=blue_size - 15
+
+    yellow_turtle.draw_circle(yellow_size)
+    yellow_turtle.pen_up()
+    yellow_turtle.right(45)
+    yellow_turtle.forward(10)
+    yellow_turtle.left(45)
+    yellow_turtle.pen_down()
+    yellow_size-yellow_size+ 10
